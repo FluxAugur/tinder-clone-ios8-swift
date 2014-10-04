ea1c7793ff3abcf91989145d60f9b8190e324105
Nathanial L. McConnell <FluxAugur@gmail.com>
2014-10-04 18:22:44 -0400

    Initial Commit

---
+++ Tinder Clone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tinder Clone
+//
+//  Created by Nathanial L. McConnell on 10/4/14.
+//  Copyright (c) 2014 Nathanial L. McConnell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
